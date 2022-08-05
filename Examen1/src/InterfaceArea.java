a621fa552b0f83b0d7d7d065f84b263b6beedce3
JessiRanOr <110475656+JessiRanOr@users.noreply.github.com>
2022-08-05 14:50:06 -0500

    Delete Examen1/src directory

--- Examen1/src/InterfaceArea.java
+++
@@ -1,7 +0,0 @@
-//Una interface contiene solo métodos abstractos,y no necesitan ser especificados.
-//Una Interface no se hereda, solo se implementa.
-public interface InterfaceArea {
-	
-	public void calculaArea();
-	
-}
